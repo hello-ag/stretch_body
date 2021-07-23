c6d448718ba299e07c61bc23a58a0439551e9f69
hello-ag <aarju@hello-robot.com>
2021-07-23 11:30:17 -0700

    New format for giving remote testing in ssh connection

--- .github/workflows/ssh_commands_test.sh
+++ .github/workflows/ssh_commands_test.sh
@@ -1,7 +1,12 @@
-cd repos/stretch_body;
+cd repos/stretch_body/body
+ls
+
 declare -a TestFiles=("test_arm" "test_dxl_comms" "test_end_of_arm" "test_hello_utils" "test_robot_params" "test_timing_stats" "test_base" "test_dynamixel_hello_XL430" "test_end_of_arm_tools" "test_lift" "test_robot" "test_timing_stats" "test_arm" "test_device" "test_dynamixel_XL430" "test_hello_utils" "test_pimu" "test_steppers" )
+declare -a TestFiles2=("test_hello_utils")
  
 # Iterate the string array using for loop
-for fileName in ${TestFiles[@]}; do
-   echo $fileName
+for fileName in ${TestFiles2[@]}; do
+   python -m unittest test.$fileName
 done
+exit
+~^Z
