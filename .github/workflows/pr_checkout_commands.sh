c7b4d39e8c9bfea015449aa1301b28f789f45a42
hello-ag <aarju@hello-robot.com>
2021-09-01 09:57:13 -0700

    Change arguments from positional to flags

--- .github/workflows/pr_checkout_commands.sh
+++ .github/workflows/pr_checkout_commands.sh
@@ -1,8 +1,15 @@
+while getopts p:b: flag
+do
+    case "${flag}" in
+        p) pr_number=${OPTARG};;
+        b) branch_name=${OPTARG};;
+    esac
+done
 cd repos
 git clone https://github.com/hello-ag/stretch_body.git stretch_body_PR_testing
 cd stretch_body_PR_testing
-git fetch origin pull/$1/head:$2
-git checkout $2
+git fetch origin pull/$pr_number/head:$branch_name
+git checkout $branch_name
 cd body
 declare -a TestFiles=("test_arm" "test_dxl_comms" "test_end_of_arm" "test_hello_utils" "test_robot_params" "test_timing_stats" "test_base" "test_dynamixel_hello_XL430" "test_end_of_arm_tools" "test_lift" "test_robot" "test_timing_stats" "test_arm" "test_device" "test_dynamixel_XL430" "test_hello_utils" "test_pimu" "test_steppers" )
 declare -a TestFiles2=("test_hello_utils")
